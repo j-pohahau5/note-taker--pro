e5d19a7f9ec8735468c1bdac9a0f053f2a69114c
Jonathan Pohahau <j.pohahau@outlook.com>
2023-01-29 09:15:46 -0800

    added new file uuid to get me an id for notes, and psuedo coded my post instance function

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const fs = require('fs'); 
 const path = require('path');
+const uuid = require('./helpers/uuid');
 
+let db = require('./db/db.json')
 const app = express();
 const PORT = 3001;
 
@@ -17,9 +19,30 @@ app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => {
+//    fs.read('./db/db.json',) 
+    res.json(db)
+})
+
+app.post('/api/notes', (req, res) => {
+    // create and import uuid
+    // have the uuid create an id for new note
+        // new note will be req.body
+    // once req.body has a new id we want to  push our new note in to db array
+    // now we need the new variable db to rewrite our new note
+    // use the write file method to do so
+    // need a respond with the new note
+})
+
+
+
+// * this means anything
+// any route under this will go to this instead of this
+// has to be the last route period
 app.get('*', (req, res) => 
 res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
 
 
 app.listen(PORT, () =>
